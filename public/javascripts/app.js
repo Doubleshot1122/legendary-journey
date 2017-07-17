1a2be960514bb3cd29642815021e0e95a693c676
Doubleshot1122 <skibo1122@yahoo.com>
2017-07-16 21:44:57 -0700

    created pie chart for company contribution

---
+++ public/javascripts/app.js
@@ -0,0 +1,43 @@
+// Load the Visualization API and the corechart package.
+ google.charts.load('current', {'packages':['corechart']});
+
+ // Set a callback to run when the Google Visualization API is loaded.
+ google.charts.setOnLoadCallback(drawChart);
+
+ // Callback that creates and populates a data table,
+ // instantiates the pie chart, passes in the data and
+ // draws it.
+ function drawChart() {
+
+   // Create the data table.
+   var data = new google.visualization.DataTable();
+   data.addColumn('string', 'Projects');
+   data.addColumn('number', 'Slices');
+   data.addRows([
+     ['All Projects', 20],
+     ['Synergy', 3],
+     ['Grow Sales', 2],
+     ['Initative 1', 3]
+   ]);
+
+   var contribution = Math.round(((8/28)*100));
+   // Set chart options
+   var options = {
+                'chartArea': {width: '100%', height: '90%'},
+                 'backgroundColor': 'transparent',
+                 'pieSliceTextStyle':{'color': 'black', 'fontSize': '18'},
+                  'width':435,
+                  'height':400,
+                  'legend':'bottom',
+                  'pieSliceText': 'percentage',
+                 'is3D':true,
+                  'slices':{
+                     0: {'color': 'transparent',
+                        'textStyle': {'color': '#73CEF5'}},
+                  }
+   };
+
+   // Instantiate and draw our chart, passing in some options.
+   var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+   chart.draw(data, options);
+ }
